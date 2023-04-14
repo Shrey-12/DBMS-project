d50327028582e92ff1dce2d41620858af2f5553a
Shreya <shreya120malik@gmail.com>
2023-04-14 09:40:54 +0530

    seller page updated

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ def build_app():
 app = build_app()
 from application.controllers import *
 if __name__ == "__main__":
-    app.run(debug=True, port=8001)
+    app.run(host="0.0.0.0",debug=True, port=8000)
