e40507fe8cc2186a8389b5522a697b923c479ca3
tiwaripari <tiwaripari13oct@gmail.com>
2023-04-16 23:29:51 +0530

    edit in agentinfosale

--- application/controllers_seller.py
+++ application/controllers_seller.py
@@ -12,14 +12,15 @@ from datetime import date
 
 @app.route('/owners')
 def owners():
-    return render_template("seller.html")
+    query = text("Select * from agent")
+    result = db.session.execute(query)
+    return render_template("seller.html", agents = result)
 
 @app.route('/owners/submit', methods = ['POST', 'GET'])
 def owners_submit():
     if request.method == "POST":
         max_existing_pid = db.session.query(db.func.max(Property.pid)).scalar()
         new_pid = max_existing_pid + 1 if max_existing_pid is not None else 1
-        print(str(new_pid) + "hahah")
         new_property = Property(
             pid         = new_pid,
             house_no    = request.form['house-no'],
@@ -41,4 +42,6 @@ def owners_submit():
         return f"Form submitted successfully."
 
     else:
-      return render_template("seller.html")
+      query = text("Select * from agent")
+      result = db.session.execute(query)
+      return render_template("seller.html", agents = result)
